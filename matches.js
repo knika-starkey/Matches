8f6bf9d1f9dfd4b279c3f79932d01fe840ac1567
knika-starkey <knika787@gmail.com>
2021-10-21 21:50:19 +0300

    add show()

--- matches.js
+++ matches.js
@@ -1,36 +1,39 @@
 let n = 13;
-let str = ' ';
-for (let i = 0; i < n; i++) {
+let str = " ";
+function show(n) {
+  for (let i = 0; i < n; i++) {
     str += "| ";
+  }
+  alert(n + str);
 }
-alert(n + str);
+
 while (n > 0) {
-    alert("Your turn!");
-    let m = prompt("Get the matches!");
-    if (isNaN(m) || m <= 0 || m > 3) {
-        alert("It's not a number!")
-        break;
-    }
-    n -= m;
-    str = ' ';
-    for (let i = 0; i < n; i++) {
-        str += "| ";
-    }
-    alert(n + str);
-    if (n <= 0) {
-        alert("You win!");
-        break;
-    }
-    alert("My turn!");
-    let rand = Math.floor(Math.random() * 3) + 1;
-    n -= rand;
-    str = ' ';
-    for (let i = 0; i < n; i++) {
-        str += "| ";
-    }
-    alert(n + str);
-    if (n <= 0) {
-        alert("You lost");
-        break;
-    }
+  alert("Your turn!");
+  let m = prompt("Get the matches!");
+  if (isNaN(m) || m <= 0 || m > 3) {
+    alert("It's not a number!");
+    break;
+  }
+  n -= m;
+  str = " ";
+  for (let i = 0; i < n; i++) {
+    str += "| ";
+  }
+  alert(n + str);
+  if (n <= 0) {
+    alert("You win!");
+    break;
+  }
+  alert("My turn!");
+  let rand = Math.floor(Math.random() * 3) + 1;
+  n -= rand;
+  str = " ";
+  for (let i = 0; i < n; i++) {
+    str += "| ";
+  }
+  alert(n + str);
+  if (n <= 0) {
+    alert("You lost");
+    break;
+  }
 }
